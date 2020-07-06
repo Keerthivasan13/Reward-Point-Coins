2d9a82129e8f28b808bb1df7f802a383682e8c63
Keerthivasan13 <Keerthivasan.s13@users.noreply.github.com>
2020-07-06 10:15:32 -0700

    Latest commit

--- contract_interact.js
+++ contract_interact.js
@@ -9,7 +9,7 @@ let web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('HTTP://127.0.0.1:7475'));
 
 // Intialize the contract address for the Smart Contract RewardContract.sol
-let contractAddress = "0x6355b14d40280e5D37eD10a0D6F87Ae4517c2440";
+let contractAddress = "0x631FE836E7295b784304c1876F128C50B2217AeA";
 
 // Read the compiled artifacts necessary to access the contract
 let jsonStr = fs.readFileSync('build/contracts/RewardContract.json', 'utf8');
@@ -18,16 +18,16 @@ let abi = JSON.parse(jsonStr).abi;
 //Initialize the variables to be used in the program
 const maxMerchants = 5;
 const maxUsers = 5;
-let addresses = ["0x1A68F413B3680e289204A6A4A6A7123dc9F05F07",
-"0xbF8F8Bc142A96e69a47889a63Da0714000C30E2E",
-"0x342C3C417E4746942aEf2De36181ec9ea93f9102",
-"0xbb28b416351440Be4BD5620f0109A283C32ef9F5",
-"0xB4f3ad256d6a4fdD3960feCf8563FD07Df191666",
-"0x0d870F51aEe90b353a4f0098a46be99AAaE1e3e4",
-"0x7b8dC35B207774c36c7E0947f5E58544B56Fba88",
-"0x658f8EBB7CD94EF06D6F5d04f97948B55a449d29",
-"0x7Ea35da7217B600A9eD1fC2d99c4f21F27FF4fA6",
-"0xf3dFcea6606BbD0370De513F97e3Cb3e894Bf68d"]
+let addresses = ["0x5c77be80E44A6bc5A65abD8c95eA9B2716533E8F",
+"0xc12b27ce60Fc45559977bA434e231b050Ec858Ba",
+"0x4A39F7c9219C5B2E680f643be432704ebC4E31ba",
+"0x81644F6286F3eF37F9da54145680A342986fac37",
+"0x1Fa12dFEc068d20547344fd6594d7f941E6Dc947",
+"0xdB589F81285BB75D57E94bB913DbC2bBC7f1c011",
+"0x251bFA85b8928E35Fa589B309f5730746cBfdD84",
+"0x6944a7Eb52EaB5b5BC9Bec8fA6B3e7490CA534EF",
+"0xe3DF1de6fB5736eE1553820EceA60d88D9D0E0c9",
+"0x0aC0c692AB982EB25059AB049ac411EDd640E62e"]
 let merchantInfoList = []
 let userInfoList = []
 let reward = new web3.eth.Contract(abi, contractAddress);
@@ -40,7 +40,6 @@ sendMainTransaction()
     .catch(function(error) {
         console.log(error);
     })
-
 
 async function registerUsers() {
     for(var i = 0; i < userInfoList.length; i++) {
@@ -98,13 +97,38 @@ async function useRewardPointsAtMerchant(userIndex, merchantIndex, rewardPoint) 
     console.log("User " + userInfoList[userIndex].name + " used "+ rewardPoint.amount +" reward points of category " + rewardPoint.scope + " and permissions "+ rewardPoint.usePermissions +" at "+ merchantInfoList[merchantIndex].name);
 }
 
-function loadMerchants() {
-    let merchantList = ["Merchant1", "Merchant2", "Merchant3", "Merchant4", "Merchant5"]
+async function loadMerchants() {
+    let loadedMerchants = new Set()
 
-    for (var i = 0; i < maxMerchants; i++) {
-        var merchantInfo = {name: merchantList[i], address: addresses[i]}
-        merchantInfoList.push(merchantInfo)
-    }
+        // Parse the json file to get merchant names and IDs.
+        var fs = require('fs');
+        await fs.readFile('merchants.json', 'utf8', function readFileCallback(err, data){
+            if (err){
+                console.log(err);
+            } else {
+                obj = JSON.parse(data); //now it's an object
+                var index = 0;
+                var z = 0;
+                console.log("Getting the Merchant Details from the Visa API Static store\n");
+                for (var i = 0; i < obj.Offers.length; i++) {
+                    var merchants = obj.Offers[i].merchantList;
+                    for (var j = 0; j < merchants.length; j++) {
+                        var name = merchants[j].merchant;
+                        var id = merchants[j].merchantId;
+                        if (!(loadedMerchants.has(id))) {
+                            let json = {name: name, id: id, address: addresses[z]}
+                            console.log("Merchant - "+ name + " with ID "+ id + " is loaded with the address "+ addresses[z])
+                            z += 1
+                            merchantInfoList.push(json)
+                            loadedMerchants.add(id)
+                        }
+
+                    }           
+                }
+            }
+            
+            console.log("-----Registered the Merchants-----\n");
+        });
 }
 
 function loadUsers() {
@@ -118,37 +142,36 @@ function loadUsers() {
 
 async function sendMainTransaction() {
     console.log("----- Reward Point Coins using Ethereum -----\n");
-    loadMerchants()
     loadUsers()
-    registerMerchants()
-    .then(function() {
-        console.log("-----Registered the Merchants-----\n");
-        registerUsers()
+    loadMerchants().then(function() {
+        registerMerchants()
         .then(function() {
-            earnAllRewardPoints()
+            registerUsers()
             .then(function() {
-                console.log("-----Gave Reward Points to the Users-----\n");
-                useAllRewardPoints()
+                earnAllRewardPoints()
                 .then(function() {
-                    console.log("-----Used Reward Points at the Merchants-----\n");
+                    console.log("-----Gave Reward Points to the Users-----\n");
+                    useAllRewardPoints()
+                    .then(function() {
+                        console.log("-----Used Reward Points at the Merchants-----\n");
+                    })
+                    .catch(function(error) {
+                        console.log(error);
+                    })
                 })
                 .catch(function(error) {
                     console.log(error);
                 })
+                console.log("-----Registered the Users-----\n");
             })
             .catch(function(error) {
                 console.log(error);
             })
-            console.log("-----Registered the Users-----\n");
         })
         .catch(function(error) {
             console.log(error);
         })
     })
-    .catch(function(error) {
-        console.log(error);
-    })
-
 
 }
 
